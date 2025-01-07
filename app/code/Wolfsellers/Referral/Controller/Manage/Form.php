41e18002bc8104b153096c75a2003f3af9fc0d43
Rafael Contreras <rjcch90@outlook.com>
2025-01-07 14:52:02 -0600

    Se crea controlador para form del Referral.

---
+++ app/code/Wolfsellers/Referral/Controller/Manage/Form.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Wolfsellers\Referral\Controller\Manage;
+
+use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
+use Magento\Customer\Controller\AccountInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Psr\Log\LoggerInterface;
+
+class Form implements AccountInterface, HttpGetActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    protected $logger;
+
+    /**
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        PageFactory $resultPageFactory,
+        LoggerInterface $logger
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Default customer account page
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->set(__('New Referral'));
+        return $resultPage;
+    }
+}
