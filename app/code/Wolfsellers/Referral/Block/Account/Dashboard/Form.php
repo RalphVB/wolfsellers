acba161b0538d51068f8313650a2bac60f7a474f
Rafael Contreras <rjcch90@outlook.com>
2025-01-07 15:28:30 -0600

    Se agrega metodo para regresar al Index de Referral en Form.

--- app/code/Wolfsellers/Referral/Block/Account/Dashboard/Form.php
+++ app/code/Wolfsellers/Referral/Block/Account/Dashboard/Form.php
@@ -6,13 +6,30 @@ use Magento\Backend\Block\Template\Context;
 
 class Form extends \Magento\Framework\View\Element\Template
 {
+
+    /**
+     * Dependency Injection
+     * 
+     * @param Context $context
+     * @param array $data
+     */
     public function __construct(Context $context, array $data = [])
     {
         parent::__construct($context, $data);
     }
     
+    /**
+     * Get Save URL
+     */
     public function getFormAction()
     {
         return $this->getUrl('referral/manage/save', ['_secure' => true]);
+    }
+
+    /**
+     * Get Referral Index URL
+     */
+    public function getBackUrl() {
+        return $this->getUrl('referral/manage');
     }
 }
